c8842382a1be6228428cc38eef8ea48a01068582
Julian Garcia <julian.garcia@monash.edu>
2015-12-07 11:35:33 +1100

    Boilerplate code

--- src/python/selfish_routing.py
+++ src/python/selfish_routing.py
@@ -1,3 +1,23 @@
 
-def my_function():
-    pass
+import numpy as np
+
+
+class MoranProcess:
+
+    def __init__(self, initial_population, population_size):
+        """
+
+        :return:
+        """
+        self.population = np.array()
+
+
+    def step(self):
+        # compute fitness
+
+        # select randomly an individual to reproduce (fitness proportional)
+
+        # select randomly an individual to die
+
+        # replace, maybe mutation
+
