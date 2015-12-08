43d725d46e565f1b8a61bc76e391ade215d884f9
najamxaidi <najam.xaidi@gmail.com>
2015-12-08 22:12:04 +1100

    The code has some errors. Tomorrow I will try to finish most of the code. It would be ready hopefully by tomorrow day end.

--- src/python/selfish_routing.py
+++ src/python/selfish_routing.py
@@ -4,15 +4,37 @@ import numpy as np
 
 class MoranProcess:
 
-    def __init__(self, initial_population, population_size):
-        """
+    def __init__(self, num_of_species, population_size):  # removed initial_population
 
+        """
         :return:
         """
-        self.population = np.array()
+        i = 0
+        self.population = np.array([])
+        while (i < population_size):
+            gen_ran_num = np.random.uniform(1, num_of_species, 1)  # generates a random number with equal probablity
+            np.append(self.population, gen_ran_num)
 
+
+    def __compute_fitness (self):
+        return
+
+    def __to_reproduce (self):
+        return
+
+    def __to_die (self):
+        return
+
+    def __mutate (self):
+        return
+
+    def __replace (self):
+        return
 
     def step(self):
+
+        var = self.population
+        print (var)
         # compute fitness
 
         # select randomly an individual to reproduce (fitness proportional)
@@ -21,3 +43,6 @@ class MoranProcess:
 
         # replace, maybe mutation
 
+
+test = MoranProcess(3,10)
+test.step()
