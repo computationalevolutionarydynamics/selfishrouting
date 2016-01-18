52d3e2f360cc28e0c5d5b1123c474a7327183a54
najamxaidi <najam.xaidi@gmail.com>
2016-01-19 10:44:43 +1100

    create graph enumerate strategies and payoff ( completed) traffic_edge(remaining)

--- src/python/graphs.py
+++ src/python/graphs.py
@@ -19,7 +19,7 @@ class test:
 
     def __create_graph(self):
         self.G.add_edge("S", "A", object=self.linear)
-        self.G.add_edge("S", "B", object=self.quadratic)
+        self.G.add_edge("S", "B", object=self.linear)
         self.G.add_edge("A", "B", object=self.constant)
         self.G.add_edge("A", "T", object=self.linear)
         self.G.add_edge("B", "T", object=self.linear)
@@ -29,18 +29,31 @@ class test:
     def __enumerate_strategies_and_payoffs(self, number_of_players):
         for i in nx.all_simple_paths(self.G, "S", "T"):
             self.strategy_set.append(i)
-        #temp_list = list(enumerate(self.strategy_set))
-        temp_list = list(self.strategy_set)
-        print(temp_list)
+        list_of_strategies = list(self.strategy_set)
+        print(list_of_strategies)
 
         temp_string = ""
-        for i in range(0,len(temp_list)):
+        for i in range(0,len(list_of_strategies)):
             temp_string += str(i)
 
-        temp = [''.join(i) for i in it.product(temp_string, repeat=number_of_players)]
-        print(temp)
+        player_options = [''.join(i) for i in it.product(temp_string, repeat=number_of_players)]
+        print(player_options)
+
+        for i in player_options:
+            for j in range(0,len(i)):
+                for k in range(0,len(list_of_strategies[int(i[j])])-1):
+                    #print(list_of_strategies[int(i[j])][k])
+                    #print(list_of_strategies[int(i[j])][k+1])
+                    self.traffic_edge(list_of_strategies[int(i[j])][k], list_of_strategies[int(i[j])][k+1], i)
+            break
 
 
+
+
+    def traffic_edge(self, node1, node2, profile = []):
+        print(profile)
+        print(node1)
+        print(node2)
 
 
     def test_func(self):
