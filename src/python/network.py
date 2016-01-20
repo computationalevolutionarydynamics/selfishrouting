Fixed test for braess network game calculation

src/python/network.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import itertools as it
+import numpy as np
 
 
 class Network:
@@ -12,7 +13,7 @@ class Network:
         self.strategy_set = []
         for i in nx.all_simple_paths(self.graph, "S", "T"):
             self.strategy_set.append(i)
-
+        self.strategy_set.sort()
         # Then, create all possible profiles corresponding to the set of strategies
         enumeration = []
         for i, strategy in enumerate(self.strategy_set):
@@ -23,17 +24,17 @@ class Network:
         # where keys are profiles (tuples of int), and values
         # are payoffs
 
-        payoff = []
-        self.profiles_with_payoffs = {}
+        payoff_temp = []
+        self.payoff = {}
         for i in self.profiles:
             for j in range(0,len(i)):
-                payoff.append(self.traffic_edge(self.graph, self.strategy_set[int(i[j])], i, j))
-            self.profiles_with_payoffs[i] = payoff[:]
-            payoff.clear()
+                payoff_temp.append(self.traffic_edge(self.graph, self.strategy_set[int(i[j])], i, j))
+            self.payoff[i] = np.array(payoff_temp[:])
+            payoff_temp.clear()
 
-        print(self.profiles)
-        print(self.strategy_set)
-        print(self.profiles_with_payoffs)
+        # print(self.profiles)
+        # print(self.strategy_set)
+        # print(self.payoff)
    # Latency functions
 
     @staticmethod

src/python/test_network.py
@@ -1,14 +1,36 @@
 import unittest
-from network import Network
+import network as network
 import networkx as nx
+import numpy as np
 
 
 class TestNetwork(unittest.TestCase):
 
-    def test_Braess_network(self):
-        t1 = self.createBraessNetwork()
-        t1.enumerate_strategies_and_payoffs(2)
-        self.assertTrue(True)
+    def test_braess_network(self):
+        my_graph = network.create_braess_network()
+        my_network_game = network.Network(my_graph, 2)
+        #print(my_network_game.payoff)
+        print(my_network_game.strategy_set)
+        profile = (0, 0)
+        payoff_expected = np.array([4,4])
+        computed_payoff =  my_network_game.payoff[profile]
+        np.testing.assert_almost_equal(payoff_expected, computed_payoff, decimal=3, err_msg=
+                                       "Payoff for profile {} should be {}, but it is {}".format(profile, payoff_expected, computed_payoff))
+
+        profile = (0, 1)
+        payoff_expected = np.array([3,3])
+        computed_payoff =  my_network_game.payoff[profile]
+        np.testing.assert_almost_equal(payoff_expected, computed_payoff, decimal=3, err_msg=
+                                       "Payoff for profile {} should be {}, but it is {}".format(profile, payoff_expected, computed_payoff))
+
+
+        profile = (0, 2)
+        payoff_expected = np.array([2,2])
+        computed_payoff =  my_network_game.payoff[profile]
+        np.testing.assert_almost_equal(payoff_expected, computed_payoff, decimal=3, err_msg=
+                                       "Payoff for profile {} should be {}, but it is {}".format(profile, payoff_expected, computed_payoff))
+
+
 
         #nx.draw(self.graph)
         #plt.show()